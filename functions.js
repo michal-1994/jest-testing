e33218f688d18551342bf5128da085b201f96b24
Michal <michalgrzegorczyk04@gmail.com>
2024-02-08 09:27:10 +0100

    add other matchers

--- functions.js
+++ functions.js
@@ -1,5 +1,14 @@
 const functions = {
 	add: (num1, num2) => num1 + num2,
+	isNull: () => null,
+	checkValue: (x) => x,
+	createUser: () => {
+		const user = {
+			firstName: 'Michał',
+		};
+		user['lastName'] = 'Grzegorczyk';
+		return user;
+	},
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,46 @@
 const functions = require('./functions');
 
+// toBe
 test('Adds 2 + 2 to equal 4', () => {
 	expect(functions.add(2, 2)).toBe(4);
+});
+
+test('Adds 2 + 2 to NOT equal 5', () => {
+	expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// toBeNull
+test('Should be null', () => {
+	expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalse
+test('Should be falsy', () => {
+	expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// toEqual
+test('User should be Michał Grzegorczyk object', () => {
+	expect(functions.createUser()).toEqual({
+		firstName: 'Michał',
+		lastName: 'Grzegorczyk',
+	});
+});
+
+// Less than and greater than
+test('Should be under 1600', () => {
+	const load1 = 800;
+	const load2 = 800;
+	expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+// Regex
+test('There is no I in team', () => {
+	expect('team').not.toMatch(/I/);
+});
+
+// Arrays
+test('Admin should be in usernames', () => {
+	const usernames = ['Michał', 'Łukasz', 'Adrian'];
+	expect(usernames).toContain('Michał');
 });
