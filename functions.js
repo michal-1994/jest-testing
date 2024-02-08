0dfb4c628f3063262c9bf6b6d4e7f8366a5515b8
Michal <michalgrzegorczyk04@gmail.com>
2024-02-08 10:30:14 +0100

    add beforeEach for two tests

--- functions.js
+++ functions.js
@@ -9,6 +9,22 @@ afterAll(() => closeDatabase());
 const initDatabase = () => console.log('Database Initialized...');
 const closeDatabase = () => console.log('Database Closed...');
 
+const nameCheck = () => console.log('Checking Name...');
+
+describe('Checking Names', () => {
+	beforeEach(() => nameCheck());
+
+	test('User is Jeff', () => {
+		const user = 'Jeff';
+		expect(user).toBe('Jeff');
+	});
+
+	test('User is Karen', () => {
+		const user = 'Karen';
+		expect(user).toBe('Karen');
+	});
+});
+
 const functions = {
 	add: (num1, num2) => num1 + num2,
 	isNull: () => null,
